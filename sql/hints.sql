0c73814c66cfadf139b068897d1a8b14d54c5cb7
Ian <ian@darwinsys.com>
2015-11-25 20:33:21 -0500

    Add another hint, and, better name for file.

--- sql/hints.sql
+++ sql/hints.sql
@@ -7,4 +7,5 @@ COPY hint(id, hint) FROM stdin;
 1	If a task seems formidable, break it down into smaller ones.
 2	Be honest with yourself; if you''re really never going to do a task, delete it!
 3	Start every day with a look at your TodoMore list; it can help organize your day.
+4	If a task can be done in a minute or two, do it now rather than entering it into TodoMore and have to do it later!
 \.
