1c7ef179d5b424c3cee82d95cb5930b1c483c487
Ian <ian@darwinsys.com>
2015-11-27 12:29:20 -0500

    typo

--- sql/hints.sql
+++ sql/hints.sql
@@ -7,7 +7,7 @@ COPY hint(id, hint, author) FROM stdin;
 1	If a task seems formidable, break it down into smaller ones.	
 2	Be honest with yourself; if you''re really never going to do a task, delete it!	--Ian
 3	Start every day with a look at your TodoMore list; it can help organize your day.	
-4	If a task can be done in a minute or two, do it now rather than entering it into TodoMore and have to do it later!	--Ian
+4	If a task can be done in a minute or two, do it now rather than entering it into TodoMore and have to do it later!	Ian
 5	Try to keep no more than 10 top-priority items, to avoid getting overwhelmed	
-6	Tell us how we can improve TodoMore. Better yet, fix it and send us the code via a GitHub "pull request".
+6	Tell us how we can improve TodoMore. Better yet, fix it and send us the code via a GitHub "pull request".	Ian
 \.
