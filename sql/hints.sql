e29d3972cac606eb8940ab42502cc4ec652e326b
Ian <ian@darwinsys.com>
2015-11-26 22:26:44 -0500

    moar

--- sql/hints.sql
+++ sql/hints.sql
@@ -3,9 +3,9 @@
 
 delete from hint;
 
-COPY hint(id, hint) FROM stdin;
-1	If a task seems formidable, break it down into smaller ones.
-2	Be honest with yourself; if you''re really never going to do a task, delete it!
-3	Start every day with a look at your TodoMore list; it can help organize your day.
-4	If a task can be done in a minute or two, do it now rather than entering it into TodoMore and have to do it later!
+COPY hint(id, hint, author) FROM stdin;
+1	If a task seems formidable, break it down into smaller ones.	
+2	Be honest with yourself; if you''re really never going to do a task, delete it!	--Ian
+3	Start every day with a look at your TodoMore list; it can help organize your day.	
+4	If a task can be done in a minute or two, do it now rather than entering it into TodoMore and have to do it later!	--Ian
 \.
